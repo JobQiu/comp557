a18eec0acebe481c388dc15e6ce5d1b26879b76b
nekitmm <mm95@rice.edu>
2016-09-03 12:05:57 -0500

    part 3

--- hw1/submission.py
+++ hw1/submission.py
@@ -1,5 +1,4 @@
-import re, util, collections
-# added import of collections module
+import re, util
 
 ############################################################
 # Problem 1a: UCS test case
@@ -9,12 +8,11 @@ import re, util, collections
 # util.createSearchProblemFromString.
 def createUCSTestCase(n):
     # BEGIN_YOUR_CODE (around 5 lines of code expected)
-
-    s = "beg mid 2"
+    case = "A B 3\nA C1 2"
     for i in range(2, n):
-	s = s + "\nbeg " + str(i) + " 2"
-    return util.createSearchProblemFromString("beg", "end", s + "\nmid end 2")
-
+        case += "\nC"+ str(i) + " " + "C" + str(i + 1) + " 1"
+    case += "\nB C 2"
+    return util.createSearchProblemFromString("A", "C", case)
     # END_YOUR_CODE
 
 ############################################################
@@ -28,24 +26,21 @@ def createUCSTestCase(n):
 def astarReduction(problem, heuristic):
     class NewSearchProblem(util.SearchProblem):
         # Please refer to util.SearchProblem to see the functions you need to
-        # override.
+        # overried.
         # BEGIN_YOUR_CODE (around 9 lines of code expected)
-	
-	def __init__(self): self.start = problem.startState()
-	# Return the start state.
-	def startState(self): return self.start
-
-	# Return whether |state| is a goal state or not.	
-	def isGoal(self, state): return problem.isGoal(state) ### not sure if correct
-	
-	# Return a list of (action, newState, cost) tuples corresponding to edges
-	# coming out of |state|.
-	def succAndCost(self, state):
-	    edges = []
-	    for (action, newState, cost) in problem.succAndCost(state):	    		
-		edges.append((action, newState, cost + heuristic(newState) - heuristic(state)))
-	    return edges
-	    
+        def __init__(self): self.start = problem.startState()
+        
+        def startState(self):
+            return self.start
+        
+        def isGoal(self, state):
+            return problem.isGoal(state)
+            
+        def succAndCost(self, state):
+            edges = []
+            for (action, newState, cost) in problem.succAndCost(state):
+                edges.append((action, newState, cost + heuristic(newState) - heuristic(state)))
+            return edges
         # END_YOUR_CODE
     newProblem = NewSearchProblem()
     return newProblem
@@ -76,17 +71,18 @@ class DeliveryProblem(util.SearchProblem):
     # |scenario|: delivery specification.
     def __init__(self, scenario):
         self.scenario = scenario
-
+        self.stats = list([0] * self.scenario.numPackages)
+        self.start = (self.scenario.truckLocation, tuple(self.stats))
     # Return the start state.
     def startState(self):
         # BEGIN_YOUR_CODE (around 1 line of code expected)
-        raise Exception("Not implemented yet")
+        return self.start
         # END_YOUR_CODE
 
     # Return whether |state| is a goal state or not.
     def isGoal(self, state):
         # BEGIN_YOUR_CODE (around 2 lines of code expected)
-        raise Exception("Not implemented yet")
+        return state[0] == self.start[0] and list(state[1]) == [2] * self.scenario.numPackages
         # END_YOUR_CODE
 
     # Return a list of (action, newState, cost) tuples corresponding to edges
@@ -96,7 +92,34 @@ class DeliveryProblem(util.SearchProblem):
         # at that location. In order for the simulation code to work, please use
         # the exact strings 'Pickup' and 'Dropoff' for those two actions.
         # BEGIN_YOUR_CODE (around 18 lines of code expected)
-        raise Exception("Not implemented yet")
+        edges = []
+        # find out type of our location
+        if(state[0] in self.scenario.pickupLocations):
+            # we are on the pickup location
+            for i in range(self.scenario.numPackages):
+                if self.scenario.pickupLocations[i] == state[0]:
+                    n = i
+            if(list(state[1])[n] == 0):
+                new_stat = list(state[1])
+                new_stat[n] = 1
+                edges.append(('Pickup', (state[0], tuple(new_stat)), 0))
+        elif(state[0] in self.scenario.dropoffLocations):
+            # we are on the drop-off location
+            for i in range(self.scenario.numPackages):
+                if self.scenario.dropoffLocations[i] == state[0]:
+                    n = i
+            if(list(state[1])[n] == 1):
+                new_stat = list(state[1])
+                new_stat[n] = 2
+                edges.append(('Dropoff', (state[0], tuple(new_stat)), 0))
+        # count the cost of move
+        k = 1
+        for i in range(self.scenario.numPackages):
+            if(list(state[1])[i] == 1):
+                k += 1
+        for i in self.scenario.getNeighbors(state[0]):
+            edges.append((i[0], (i[1], state[1]), k))
+        return edges
         # END_YOUR_CODE
 
 ############################################################
@@ -109,7 +132,11 @@ class DeliveryProblem(util.SearchProblem):
 def createHeuristic1(scenario):
     def heuristic(state):
         # BEGIN_YOUR_CODE (around 2 lines of code expected)
-        raise Exception("Not implemented yet")
+        k = 1
+        for i in range(len(state[1])):
+            if(state[1][i] == 1):
+                k += 1
+        return k * (abs(state[0][0] - scenario.truckLocation[0]) + abs(state[0][1] - scenario.truckLocation[1]))
         # END_YOUR_CODE
     return heuristic
 
@@ -122,7 +149,25 @@ def createHeuristic1(scenario):
 def createHeuristic2(scenario, package):
     def heuristic(state):
         # BEGIN_YOUR_CODE (around 11 lines of code expected)
-        raise Exception("Not implemented yet")
+        heuristic = 0
+        k = 1
+        for i in range(scenario.numPackages):
+            if(list(state[1])[i] == 1):
+                k += 1
+        me = state[0]
+        pickup = scenario.pickupLocations[package]
+        drop = scenario.dropoffLocations[package]
+        goal = scenario.truckLocation
+        if(state[1][package] == 0):
+            heuristic += k * (abs(me[0] - pickup[0]) + abs(me[1] - pickup[1]))
+            heuristic += (k + 1) * (abs(pickup[0] - drop[0]) + abs(pickup[1] - drop[1]))
+            heuristic += k * (abs(drop[0] - goal[0]) + abs(drop[1] - goal[1]))
+        elif(state[1][package] == 1):
+            heuristic += k * (abs(me[0] - drop[0]) + abs(me[1] - drop[1]))
+            heuristic += (k - 1) * (abs(drop[0] - goal[0]) + abs(drop[1] - goal[1]))
+        else:
+            heuristic += k * (abs(me[0] - goal[0]) + abs(me[1] - goal[1]))
+        return heuristic
         # END_YOUR_CODE
     return heuristic
 
@@ -136,5 +181,12 @@ def createHeuristic2(scenario, package):
 # createHeuristic2.
 def createHeuristic3(scenario):
     # BEGIN_YOUR_CODE (around 5 lines of code expected)
-    raise Exception("Not implemented yet")
+    far = 0
+    n = 0
+    for i in range(scenario.numPackages):
+        dist = abs(scenario.truckLocation[0] - scenario.pickupLocations[i][0]) + abs(scenario.truckLocation[1] - scenario.pickupLocations[i][1])
+        if(dist > far):
+            n = i
+            far = dist
+    return createHeuristic2(scenario, n)
     # END_YOUR_CODE
